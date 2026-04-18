8e9e6709e52cdc14c6bc84691e7932a8d023058d
rmecidli26 <rmecidli26@gmail.com>
2026-04-18 13:13:03 +0000

    9-sum_total.py

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -3,11 +3,7 @@
 def summation_i_squared(n):
     if not isinstance(n, int):
         return None
-    
     if n < 1:
         return 0
-    
-    if n == 1:
-        return 1
-    
-    return n**2 + summation_i_squared(n - 1)
+    result = (n * (n + 1) * (2 * n + 1)) // 6
+    return result
