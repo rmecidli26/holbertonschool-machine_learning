a6c90f9f03d5e6088747a857a9920ac1546b92c1
rmecidli26 <rmecidli26@gmail.com>
2026-04-18 13:33:01 +0000

    9-sum_total.py

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python3
 def summation_i_squared(n):
-    try:
-        if type(n) is not int or n < 0:
-            return None
-        if n == 0:
-            return 0
-        return (n * (n + 1) * (2 * n + 1)) // 6
-    except (TypeError, NameError, AttributeError):
+    if type(n) is not int:
         return None
-try:
-    val = n_variable_not_defined
-except NameError:
-    val = "invalid"
-print(summation_i_squared(val))
+    if n < 0:
+        return None
+    if n == 0:
+        return 0
+    return (n * (n + 1) * (2 * n + 1)) // 6
