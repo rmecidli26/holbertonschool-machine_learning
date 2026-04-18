5d89d05458c34b71a5c99ca7caa72e657ae8ed52
rmecidli26 <rmecidli26@gmail.com>
2026-04-18 13:11:26 +0000

    9-sum_total.py

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
+
 def summation_i_squared(n):
-    if not n.isdigit():
+    if not isinstance(n, int):
         return None
+    
     if n < 1:
         return 0
+    
     if n == 1:
         return 1
-    else:
-        return n**2 + summation_i_squared(n-1)
+    
+    return n**2 + summation_i_squared(n - 1)
