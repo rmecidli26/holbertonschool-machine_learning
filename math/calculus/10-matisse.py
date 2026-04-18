4328c7c523098c679351eb6aad3416d8e4760d2d
rmecidli26 <rmecidli26@gmail.com>
2026-04-18 13:42:57 +0000

    10-exercise

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python3
-
 def poly_derivative(poly):
     if type(poly) is not list or len(poly) == 0:
         return None
-    
     for coeff in poly:
         if type(coeff) is not int and type(coeff) is not float:
             return None
-
     if len(poly) == 1:
         return [0]
-
     derivative = []
     for i in range(1, len(poly)):
         derivative.append(poly[i] * i)
-
     return derivative
