7bdbf3b78878766ab87bf10367fcb7c3ee6a5eae
rmecidli26 <rmecidli26@gmail.com>
2026-04-18 13:34:39 +0000

    9-sum_total.py

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+import sys
+sys.setrecursionlimit(100000)
 def summation_i_squared(n):
-    if type(n) is not int:
-        return None
-    if n < 0:
+    if type(n) is not int or n < 0:
         return None
     if n == 0:
         return 0
-    return (n * (n + 1) * (2 * n + 1)) // 6
+    if n == 1:
+        return 1
+    return n**2 + summation_i_squared(n - 1)
