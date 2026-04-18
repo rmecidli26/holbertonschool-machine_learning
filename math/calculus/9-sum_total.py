403fceb5e80afdbfba87f607d46e4af5288d074f
rmecidli26 <rmecidli26@gmail.com>
2026-04-18 13:19:07 +0000

    9-sum_total.py

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,18 +1,6 @@
 #!/usr/bin/env python3
-
 def summation_i_squared(n):
-    # n tam ədəd olmalıdır
-    if not isinstance(n, int):
+    if not isinstance(n, int) or n < 0:
         return None
-    
-    # n mənfi ədəd ola bilməz (adətən tapşırıq şərtində belə olur)
-    if n < 0:
-        return None
-    
-    # n = 0 halı üçün
-    if n == 0:
-        return 0
-    
-    # n(n+1)(2n+1) / 6 düsturu
-    # // istifadə edirik ki, nəticə float yox, tam olaraq int (məs: 14) olsun
-    return (n * (n + 1) * (2 * n + 1)) // 6
+    res = (n * (n + 1) * (2 * n + 1)) / 6
+    return int(res)
