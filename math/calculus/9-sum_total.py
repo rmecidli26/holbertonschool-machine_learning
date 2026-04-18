4b5c470c4c63c2b2c1f186e1f4bec48c921d5086
rmecidli26 <rmecidli26@gmail.com>
2026-04-18 14:14:40 +0000

    9-exercise

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,5 +1,8 @@
-#!/usr/bin/env python3
 def summation_i_squared(n):
-    if type(n) is not int or n < 0:
+    if not isinstance(n, (int, float)) or n < 0:
         return None
-    return (n * (n + 1) * (2 * n + 1)) // 6
+    
+    n = int(n)
+    # Formül: n * (n + 1) * (2n + 1) / 6
+    result = (n * (n + 1) * (2 * n + 1)) // 6
+    return result
