1e75ecb1cdaacefa71762405a5500a602faf11ce
rmecidli26 <rmecidli26@gmail.com>
2026-04-25 18:10:00 +0000

    11-exercise

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+
+"""
+
+
+def np_transpose(matrix):
+    """
+    Bir numpy.ndarray-in transpozunu qaytarır.
+    Arqumentlər:
+        matrix: Transpoz ediləcək numpy.ndarray.
+    Qaytarır:
+        Transpoz edilmiş yeni numpy.ndarray.
+    """
+    return matrix.transpose()
