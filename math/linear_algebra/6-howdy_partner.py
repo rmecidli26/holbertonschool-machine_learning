c2d2518f8bd8398d07bd2c2863e9efa701faf761
rmecidli26 <rmecidli26@gmail.com>
2026-04-25 13:33:58 +0000

    6-exercise

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Bu modul iki siyahını birləşdirmək üçün funksiyanı ehtiva edir.
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    İki siyahını (list) birləşdirərək yeni bir siyahı qaytarır.
+    Arqumentlər:
+        arr1: Birinci siyahı.
+        arr2: İkinci siyahı.
+    Qaytarır:
+        arr1 və arr2-nin elementlərindən ibarət yeni siyahı.
+    """
+    return arr1 + arr2
