6a03e1a57c4956eef7fdfde2d79bd53bc6a47a53
rmecidli26 <rmecidli26@gmail.com>
2026-04-25 13:05:20 +0000

    3-exercise

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -2,15 +2,11 @@
 """
 Bu modul matrisin transpozunu hesablamaq üçün funksiyadir.
 """
-
-
 def matrix_transpose(matrix):
     """
     2D matrisin transpozunu qaytarır.
-    
     Arqumentlər:
         matrix: Siyahılardan ibarət 2D siyahı (list of lists).
-        
     Qaytarır:
         Yeni transpoz edilmiş matris.
     """
