d12701948f9b748fa250f1eb19989be0a59fa4a9
rmecidli26 <rmecidli26@gmail.com>
2026-04-25 18:02:10 +0000

    10-exercise

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -9,10 +9,8 @@ def np_shape(matrix):
     """
     Bir numpy.ndarray-in ölçülərini (shape) 
     bir tuple kimi qaytarır.
-    
     Arqumentlər:
         matrix: Ölçüsü hesablanacaq numpy.ndarray.
-        
     Qaytarır:
         Tam ədədlərdən ibarət tuple.
     """
