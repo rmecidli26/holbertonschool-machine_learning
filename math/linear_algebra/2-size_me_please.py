8a322087367e0c6d93e269a9c149be32c9dbc8b8
rmecidli26 <rmecidli26@gmail.com>
2026-04-25 12:56:36 +0000

    2-exercise

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,8 +1,20 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""
+Bu modul matrislərin ölçüsünü hesablamaq üçün funksiyanı ehtiva edir.
+Daxil olan siyahının (list) hər bir dərinliyini ölçür.
+"""
+
+
 def matrix_shape(matrix):
-    '''Bura bos olmalidir'''
+    """
+    Verilmiş matrisin ölçülərini (shape) bir siyahı kimi qaytarır.
+    Arqumentlər: matrix (list)
+    Qaytarır: list of integers
+    """
     shape = []
     while isinstance(matrix, list):
         shape.append(len(matrix))
+        if not matrix:
+            break
         matrix = matrix[0]
     return shape
