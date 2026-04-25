6c1dd704a32954c9414787a3fe1e12a57993cee0
rmecidli26 <rmecidli26@gmail.com>
2026-04-25 18:00:28 +0000

    10-exercise

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Bu modul bir NumPy massivinin ölçülərini (shape) hesablamaq üçün funksiyanı ehtiva edir.
+"""
+
+
+def np_shape(matrix):
+    """
+    Bir numpy.ndarray-in ölçülərini (shape) bir tuple kimi qaytarır.
+    
+    Arqumentlər:
+        matrix: Ölçüsü hesablanacaq numpy.ndarray.
+        
+    Qaytarır:
+        Tam ədədlərdən ibarət tuple.
+    """
+    return matrix.shape
