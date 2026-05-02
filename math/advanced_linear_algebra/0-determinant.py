b2ea50a0e1ef074b8dd81298f0f3828f12bcb295
rmecidli26 <rmecidli26@gmail.com>
2026-05-02 15:05:43 +0000

    0-exercise

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -3,16 +3,19 @@ import numpy as np
 
 def determinant(matrix):
     try:
+        # Siyahını NumPy massivinə çeviririk
         arr = np.array(matrix)
 
-        "gfidno"
+        # Matrisin boş olub-olmadığını yoxlayırıq
         if arr.size == 0:
             return "This matrix is empty!"
 
-        "gdmfgd"
+        # Matrisin 2 ölçülü və kvadrat olduğunu yoxlayırıq
         if arr.ndim < 2 or arr.shape[0] != arr.shape[1]:
             return "This matrix is not square"
 
+        # Determinantı hesablayıb qaytarırıq
         return np.linalg.det(arr)
     except Exception as e:
+        # Hər hansı sistem xətası baş verərsə tuturuq
         return f"System error: {e}"
