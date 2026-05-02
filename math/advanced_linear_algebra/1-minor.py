d9c86b2c0e274a40f01eb7a9a670b039520d3a04
rmecidli26 <rmecidli26@gmail.com>
2026-05-02 15:14:23 +0000

    0-exercise

---
+++ math/advanced_linear_algebra/1-minor.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+def determinant(matrix):
+    if len(matrix) == 1:
+        return matrix[0][0]
+    if len(matrix) == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
+    det = 0
+    for j in range(len(matrix)):
+        sub_matrix = [row[:j] + row[j+1:] for row in matrix[1:]]
+        det += ((-1) ** j) * matrix[0][j] * determinant(sub_matrix)
+    return det
+
+
+def minor(matrix):
+    if not isinstance(matrix, list) or not all(isinstance(row, list)
+                                               for row in matrix):
+        raise TypeError("matrix must be a list of lists")
+
+    if len(matrix) == 0 or not all(len(row) == len(matrix) for row in matrix):
+        raise ValueError("matrix must be a non-empty square matrix")
+
+    n = len(matrix)
+    if n == 1:
+        return [[1]]
+
+    minor_matrix = []
+    for i in range(n):
+        row_minors = []
+        for j in range(n):
+            sub_matrix = [row[:j] + row[j+1:]
+                          for row in (matrix[:i] + matrix[i+1:])]
+            row_minors.append(determinant(sub_matrix))
+        minor_matrix.append(row_minors)
+
+    return minor_matrix
