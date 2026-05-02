77ce9ccb9528b1b948be63ec933c32cc59520d51
rmecidli26 <rmecidli26@gmail.com>
2026-05-02 14:52:41 +0000

    0-exercise

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 import numpy as np
+
 def determinant(matrix):
     try:
         arr = np.array(matrix)
-        if arr == 0:
+        
+        
+        if arr.size == 0:
             return "This matrix is empty!"
-        if arr.shape[0] != arr.shape[1]:
+        
+        
+        if arr.ndim < 2 or arr.shape[0] != arr.shape[1]:
             return "This matrix is not square"
-        return np.linalg.det(matrix)
+            
+        return np.linalg.det(arr)
     except Exception as e:
         return f"System error: {e}"
