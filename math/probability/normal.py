a89df6c472315942b08af7edfe0387a85d5eb450
rmecidli26 <rmecidli26@gmail.com>
2026-05-09 16:14:03 +0000

    0-exercise

---
+++ math/probability/normal.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Normal distribution module
+"""
+
+
+class Normal:
+    """ Represents a normal distribution """
+
+    def __init__(self, data=None, mean=0., stddev=1.):
+        """ Initialize Normal distribution """
+        if data is None:
+            if stddev <= 0:
+                raise ValueError("stddev must be a positive value")
+            self.mean = float(mean)
+            self.stddev = float(stddev)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+
+            self.mean = float(sum(data) / len(data))
+            
+            variance = sum((x - self.mean) ** 2 for x in data) / len(data)
+            self.stddev = float(variance ** 0.5)
