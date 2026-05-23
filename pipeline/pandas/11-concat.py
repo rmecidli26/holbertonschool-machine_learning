4cc5c9cd452a8e5b808a22c16879445ff2d8466e
rmecidli26 <rmecidli26@gmail.com>
2026-05-23 08:30:15 +0000

    10-exercise

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Defines a function to concatenate two DataFrames with hierarchical keys"""
+import pandas as pd
 index = __import__('10-index').index
 
 
@@ -21,11 +22,6 @@ def concat(df1, df2):
 
     # Filter bitstamp data up to and including timestamp 1417411920
     df2_filtered = df2_indexed.loc[:1417411920]
-
-    # Dynamically extract the pandas module reference to use pd.concat
-    pd = __import__('sys').modules.get('pandas')
-    if pd is None:
-        import pandas as pd
 
     # Concatenate df2 on top of df1 with the respective keys
     return pd.concat([df2_filtered, df1_indexed], keys=['bitstamp', 'coinbase'])
