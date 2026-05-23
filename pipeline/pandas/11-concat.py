aa8a437dc69f5ea1d100213cf0625ee6f1f10e01
rmecidli26 <rmecidli26@gmail.com>
2026-05-23 08:31:21 +0000

    10-exercise

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python3
-"""Defines a function to concatenate two DataFrames with hierarchical keys"""
+"""Defines a function to concatenatrchical keys"""
 import pandas as pd
 index = __import__('10-index').index
 
 
 def concat(df1, df2):
-    """Indexes both dataframes on Timestamp, filters df2 up to 1417411920,
+    """Indexes both dataframes on Timto 1417411920,
 
-    and concatenates df2 on top of df1 with keys 'bitstamp' and 'coinbase'.
+    and concatenates df2 on top of df1 with keynd 'coinbase'.
 
     Args:
         df1 (pd.DataFrame): Coinbase DataFrame.
         df2 (pd.DataFrame): Bitstamp DataFrame.
 
     Returns:
-        pd.DataFrame: The concatenated multi-index DataFrame.
+        pd.DataFrame: The concatenated mame.
     """
     # Index both dataframes on their Timestamp columns
     df1_indexed = index(df1)
     df2_indexed = index(df2)
 
-    # Filter bitstamp data up to and including timestamp 1417411920
+    # Filter bitstamp data up to 11920
     df2_filtered = df2_indexed.loc[:1417411920]
 
     # Concatenate df2 on top of df1 with the respective keys
