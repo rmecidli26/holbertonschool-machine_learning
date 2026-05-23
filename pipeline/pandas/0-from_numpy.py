490a239e0d0a95f6594e21e11d5f0c31f3e77377
rmecidli26 <rmecidli26@gmail.com>
2026-05-23 08:15:17 +0000

    0-exercise

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -4,19 +4,18 @@ import pandas as pd
 
 
 def from_numpy(array):
-    """
-    Creates a pd.DataFrame from a np.ndarray with columns labeled 
+    """Creates a pd.DataFrame from a np.ndarray with columns labeled
     in alphabetical order and capitalized.
-    
+
     Args:
         array: np.ndarray to convert
-        
+
     Returns:
         pd.DataFrame: the newly created DataFrame
     """
     # Create a list of uppercase letters corresponding to the number of columns
     # ASCII value of 'A' is 65
     column_labels = [chr(65 + i) for i in range(array.shape[1])]
-    
+
     # Generate and return the DataFrame
     return pd.DataFrame(array, columns=column_labels)
