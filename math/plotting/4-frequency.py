6538534ef548fac1e96d9092e42474cdb52ac7d7
rmecidli26 <rmecidli26@gmail.com>
2026-05-29 10:11:56 +0000

    4-exercise

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Module to plot a histogram of student grades.
+Module to plot a histogram using bar function.
 """
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,14 +8,17 @@ import matplotlib.pyplot as plt
 
 def frequency():
     """
-    Plots a histogram of student grades with bins of 10.
+    Plots a histogram using plt.bar to match exact reference requirements.
     """
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
 
-    bins = np.arange(0, 101, 10)
-    plt.hist(student_grades, bins=bins, edgecolor='black')
+    # Məlumatları 10-luq aralıqlarla qruplaşdırırıq
+    counts, bins = np.histogram(student_grades, bins=np.arange(0, 101, 10))
+
+    # Sütunları çəkirik
+    plt.bar(bins[:-1], counts, width=10, edgecolor='black', align='edge')
 
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
