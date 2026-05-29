e1ee4d89fae73819fd1f0071095ae82f2e230360
rmecidli26 <rmecidli26@gmail.com>
2026-05-29 10:08:28 +0000

    1-exercise

--- math/plotting/2-change_scale.py
+++ math/plotting/2-change_scale.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Module to plot exponential decay with a logarithmic scale.
+Module to plot expon ential decay with a logarithmic scale.
 """
 import numpy as np
 import matplotlib.pyplot as plt
