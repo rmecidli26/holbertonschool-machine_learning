a249039033672d4292d8ec86795c58a6df220529
rmecidli26 <rmecidli26@gmail.com>
2026-05-29 10:04:32 +0000

    0-exercise

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -2,12 +2,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def line():
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
-
-    # X oxu aralığını 0-dan 10-a qədər təyin edirik
     plt.xlim(0, 10)
-    
-    # Qrafiki çəkirik: 'r-' 'red' (qırmızı) və solid (düz) xətt deməkdir
     plt.plot(y, 'r-')
