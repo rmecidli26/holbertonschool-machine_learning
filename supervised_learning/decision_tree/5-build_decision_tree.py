030aaa40fedf9716caa4d974e9f123cf4236c419
rmecidli26@gmail.com <rmecidli26@gmail.com>
2026-06-25 19:13:59 +0000

    4-exercise

--- supervised_learning/decision_tree/5-build_decision_tree.py
+++ supervised_learning/decision_tree/5-build_decision_tree.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 import numpy as np
 
-# Class daxilində metod tam olaraq belə olmalıdır:
+# Digər kodların və Node klasının daxili...
+
+class Node:
+    # Əgər əvvəlki metodlar və __init__ varsa, onların hizasında olmalıdır:
+    
     def update_indicator(self):
         def is_large_enough(x):
-            if not self.lower: return np.ones(x.shape[0], dtype=bool)
+            if not self.lower: 
+                return np.ones(x.shape[0], dtype=bool)
             return np.all([x[:, k] > v for k, v in self.lower.items()], axis=0)
 
         def is_small_enough(x):
-            if not self.upper: return np.ones(x.shape[0], dtype=bool)
+            if not self.upper: 
+                return np.ones(x.shape[0], dtype=bool)
             return np.all([x[:, k] <= v for k, v in self.upper.items()], axis=0)
 
         self.indicator = lambda x : np.all([is_large_enough(x), is_small_enough(x)], axis=0)
