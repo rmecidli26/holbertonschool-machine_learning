35f767b0c9147ee7d63293a0393714d8dddef7f7
rmecidli26@gmail.com <rmecidli26@gmail.com>
2026-06-25 18:56:50 +0000

    4-exercise

---
+++ supervised_learning/decision_tree/2-build_decision_tree.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+"""Decision Tree Visualizer"""
+
+
+class Node:
+    """Internal node"""
+
+    def __init__(
+        self,
+        feature=None,
+        threshold=None,
+        left_child=None,
+        right_child=None,
+        depth=0,
+        is_root=False,
+    ):
+        self.feature = feature
+        self.threshold = threshold
+        self.left_child = left_child
+        self.right_child = right_child
+        self.depth = depth
+        self.is_root = is_root
+        self.is_leaf = False
+
+    def count_nodes_below(self, only_leaves=False):
+        """Count nodes"""
+        lc, rc = self.left_child, self.right_child
+        left = lc.count_nodes_below(only_leaves) if lc else 0
+        right = rc.count_nodes_below(only_leaves) if rc else 0
+        return left + right + (0 if only_leaves else 1)
+
+    def left_child_add_prefix(self, text):
+        """Adds prefix for left child visualization"""
+        lines = text.split("\n")
+        new_text = "    +---" + lines[0] + "\n"
+        for x in lines[1:]:
+            if x:
+                new_text += ("    |   " + x) + "\n"
+        return new_text
+
+    def right_child_add_prefix(self, text):
+        """Adds prefix for right child visualization"""
+        lines = text.split("\n")
+        new_text = "    +---" + lines[0] + "\n"
+        for x in lines[1:]:
+            if x:
+                new_text += ("        " + x) + "\n"
+        return new_text
+
+    def __str__(self):
+        """String representation of Node"""
+        t = "root" if self.is_root else "node"
+        string = f"{t} [feature={self.feature}, threshold={self.threshold}]\n"
+        if self.left_child:
+            string += self.left_child_add_prefix(self.left_child.__str__())
+        if self.right_child:
+            string += self.right_child_add_prefix(self.right_child.__str__())
+        return string
+
+
+class Leaf:
+    """Leaf node"""
+
+    def __init__(self, value, depth=0):
+        self.value = value
+        self.depth = depth
+        self.is_leaf = True
+
+    def count_nodes_below(self, only_leaves=False):
+        """Count leaf"""
+        return 1
+
+    def __str__(self):
+        """String representation of Leaf"""
+        return f"leaf [value={self.value}]\n"
+
+
+class Decision_Tree:
+    """Tree class"""
+
+    def __init__(self, root=None):
+        self.root = root
+
+    def depth(self):
+        """Calculate depth"""
+
+        def _max_depth(node):
+            if node is None:
+                return 0
+            if node.is_leaf:
+                return node.depth
+            return max(
+                _max_depth(node.left_child), _max_depth(node.right_child)
+            )
+
+        return _max_depth(self.root)
+
+    def count_nodes(self, only_leaves=False):
+        """Count total"""
+        if self.root is None:
+            return 0
+        return self.root.count_nodes_below(only_leaves=only_leaves)
+
+    def __str__(self):
+        """String representation of Tree"""
+        if self.root is None:
+            return ""
+        return self.root.__str__().rstrip("\n")
