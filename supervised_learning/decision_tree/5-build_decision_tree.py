05586a845a618711fbf3aafce246892287955220
rmecidli26@gmail.com <rmecidli26@gmail.com>
2026-06-25 19:10:21 +0000

    4-exercise

---
+++ supervised_learning/decision_tree/5-build_decision_tree.py
@@ -0,0 +1,11 @@
+def update_indicator(self):
+
+        def is_large_enough(x):
+            if not self.lower: return np.ones(x.shape[0], dtype=bool)
+            return np.all([x[:, k] > v for k, v in self.lower.items()], axis=0)
+
+        def is_small_enough(x):
+            if not self.upper: return np.ones(x.shape[0], dtype=bool)
+            return np.all([x[:, k] <= v for k, v in self.upper.items()], axis=0)
+
+        self.indicator = lambda x : np.all([is_large_enough(x), is_small_enough(x)], axis=0)
