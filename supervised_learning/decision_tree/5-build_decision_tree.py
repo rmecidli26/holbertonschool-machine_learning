7850f69729e1e6fd596ba90139adc3b1f257d2b6
rmecidli26@gmail.com <rmecidli26@gmail.com>
2026-06-25 19:12:55 +0000

    4-exercise

--- supervised_learning/decision_tree/5-build_decision_tree.py
+++ supervised_learning/decision_tree/5-build_decision_tree.py
@@ -1,5 +1,8 @@
-def update_indicator(self):
+#!/usr/bin/env python3
+import numpy as np
 
+# Class daxilində metod tam olaraq belə olmalıdır:
+    def update_indicator(self):
         def is_large_enough(x):
             if not self.lower: return np.ones(x.shape[0], dtype=bool)
             return np.all([x[:, k] > v for k, v in self.lower.items()], axis=0)
