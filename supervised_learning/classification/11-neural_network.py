726f2d12c0cadf78fffb0039f828114cbfbf2ccd
rmecidli26 <rmecidli26@gmail.com>
2026-07-11 15:20:20 +0000

    0-exercises

---
+++ supervised_learning/classification/11-neural_network.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+"""Defines a neural network with one hidden layer performing cost calculation"""
+import numpy as np
+
+
+class NeuralNetwork:
+    """Represents a neural network with one hidden layer."""
+
+    def __init__(self, nx, nodes):
+        """Initializes the neural network."""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        self.__W1 = np.random.normal(size=(nodes, nx))
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+
+        self.__W2 = np.random.normal(size=(1, nodes))
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        """Getter for private attribute __W1"""
+        return self.__W1
+
+    @property
+    def b1(self):
+        """Getter for private attribute __b1"""
+        return self.__b1
+
+    @property
+    def A1(self):
+        """Getter for private attribute __A1"""
+        return self.__A1
+
+    @property
+    def W2(self):
+        """Getter for private attribute __W2"""
+        return self.__W2
+
+    @property
+    def b2(self):
+        """Getter for private attribute __b2"""
+        return self.__b2
+
+    @property
+    def A2(self):
+        """Getter for private attribute __A2"""
+        return self.__A2
+
+    def forward_prop(self, X):
+        """Calculates the forward propagation of the neural network."""
+        Z1 = np.dot(self.__W1, X) + self.__b1
+        self.__A1 = 1 / (1 + np.exp(-Z1))
+
+        Z2 = np.dot(self.__W2, self.__A1) + self.__b2
+        self.__A2 = 1 / (1 + np.exp(-Z2))
+
+        return self.__A1, self.__A2
+
+    def cost(self, Y, A):
+        """Calculates the cost of the model using logistic regression.
+
+        Parameters:
+        Y (numpy.ndarray): Correct labels for the input data, shape (1, m).
+        A (numpy.ndarray): Activated output of the final layer, shape (1, m).
+
+        Returns:
+        The cost as a float.
+        """
+        m = Y.shape[1]
+        cost = -1 / m * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+        return cost
