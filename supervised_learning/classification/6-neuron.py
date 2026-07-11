f845f1783cbfafc47ccf7b094fe4457270b42959
rmecidli26 <rmecidli26@gmail.com>
2026-07-11 15:06:19 +0000

    0-exercises

--- supervised_learning/classification/6-neuron.py
+++ supervised_learning/classification/6-neuron.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
-"""Defines a single neuron performing binary classification with a full train loop"""
+"""Defines a single neuron performing binary classification"""
 import numpy as np
+
+
 class Neuron:
-    """Represents a single neuron performing binary classification."""
+    """Represents a single neuron"""
+
     def __init__(self, nx):
         """Initializes the neuron."""
         if not isinstance(nx, int):
@@ -12,34 +15,41 @@ class Neuron:
         self.__W = np.random.normal(size=(1, nx))
         self.__b = 0
         self.__A = 0
+
     @property
     def W(self):
         """Getter for the private weights vector __W"""
         return self.__W
+
     @property
     def b(self):
         """Getter for the private bias __b"""
         return self.__b
+
     @property
     def A(self):
         """Getter for the private activated output __A"""
         return self.__A
+
     def forward_prop(self, X):
         """Calculates the forward propagation of the neuron."""
         Z = np.dot(self.__W, X) + self.__b
         self.__A = 1 / (1 + np.exp(-Z))
         return self.__A
+
     def cost(self, Y, A):
         """Calculates the cost of the model using logistic regression."""
         m = Y.shape[1]
         cost = -1 / m * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
         return cost
+
     def evaluate(self, X, Y):
         """Evaluates the neuron's predictions."""
         A = self.forward_prop(X)
         net_cost = self.cost(Y, A)
         prediction = np.where(A >= 0.5, 1, 0)
         return prediction, net_cost
+
     def gradient_descent(self, X, Y, A, alpha=0.05):
         """Calculates one pass of gradient descent on the neuron."""
         m = Y.shape[1]
@@ -48,8 +58,9 @@ class Neuron:
         db = (1 / m) * np.sum(dZ)
         self.__W = self.__W - (alpha * dW)
         self.__b = self.__b - (alpha * db)
+
     def train(self, X, Y, iterations=5000, alpha=0.05):
-        """Trains the neuron by running forward propagation and gradient descent."""
+        """Trains the neuron by running forward propagation and descent."""
         if not isinstance(iterations, int):
             raise TypeError("iterations must be an integer")
         if iterations <= 0:
