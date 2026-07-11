4779060e6f6f061f1e10283b9575e48ba5989123
rmecidli26 <rmecidli26@gmail.com>
2026-07-11 12:52:00 +0000

    0-exercises

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Defines a single neuron performing binary classification with private attributes
+"""
+import numpy as np
+
+
+class Neuron:
+    """
+    Represents a single neuron performing binary classification.
+    """
+    def __init__(self, nx):
+        """
+        Initializes the neuron.
+
+        Parameters:
+        nx (int): The number of input features to the neuron.
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be a integer")
+        if nx < 1:
+            raise ValueError("nx must be positive")
+
+        # Private attributes denoted by double underscores
+        self.__W = np.random.normal(size=(1, nx))
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Getter for the private weights vector __W"""
+        return self.__W
+
+    @property
+    def b(self):
+        """Getter for the private bias __b"""
+        return self.__b
+
+    @property
+    def A(self):
+        """Getter for the private activated output __A"""
+        return self.__A
