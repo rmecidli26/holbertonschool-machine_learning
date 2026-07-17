fe968f987cfc417a00f00e30a561a0b4e731b846
rmecidli26 <rmecidli26>
2026-07-17 00:19:19 +0000

    0-exercise

---
+++ supervised_learning/keras/0-sequential.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" Keras Sequential Model """
+import tensorflow as tf
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """ Tree """
+    model = tf.keras.Sequential()
+    regularizer = tf.keras.regularizers.l2(lambtha)
+
+    for i in range(len(layers)):
+        if i == 0:
+            # First layer
+            model.add(tf.keras.layers.Dense(
+                units=layers[i],
+                activation=activations[i],
+                kernel_regularizer=regularizer,
+                input_dim=nx
+            ))
+        else:
+            model.add(tf.keras.layers.Dense(
+                units=layers[i],
+                activation=activations[i],
+                kernel_regularizer=regularizer
+            ))
+
+        # Last layer
+        if i < len(layers) - 1:
+            model.add(tf.keras.layers.Dropout(rate=1 - keep_prob))
+
+    return model
