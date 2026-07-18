c8e76b170e4368cac93aaf8912ab89e0ade1c7ab
rmecidli26 <rmecidli26@gmail.com>
2026-07-18 14:30:24 +0000

    0-exercise

---
+++ supervised_learning/keras/6-train.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+""" Keras Training with Early Stopping """
+import tensorflow.keras as K
+
+
+def train_model(
+    network,
+    data,
+    labels,
+    batch_size,
+    epochs,
+    validation_data=None,
+    early_stopping=False,
+    patience=0,
+    verbose=True,
+    shuffle=False
+):
+    """ Trains a model using mini-batch gradient descent with early stopping """
+    callbacks = []
+
+    # Erkən dayandırma şərtini yoxlayırıq
+    if validation_data and early_stopping:
+        callbacks.append(K.callbacks.EarlyStopping(
+            monitor='val_loss',
+            patience=patience
+        ))
+
+    history = network.fit(
+        x=data,
+        y=labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=verbose,
+        shuffle=shuffle,
+        validation_data=validation_data,
+        callbacks=callbacks
+    )
+    return history
