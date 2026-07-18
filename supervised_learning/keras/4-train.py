1790f10da06d1f538b267515462dad8ed0bb3a3d
rmecidli26 <rmecidli26@gmail.com>
2026-07-18 12:10:57 +0000

    0-exercise

---
+++ supervised_learning/keras/4-train.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Train Model """
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs, verbose=True, shuffle=False):
+    """ Trains a model using mini-batch gradient descent """
+    # network.fit metodu modeli verilən parametrlərlə təlim edir 
+    # və geriyə təlim tarixçəsini (History obyektini) qaytarır.
+    history = network.fit(
+        x=data,
+        y=labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=verbose,
+        shuffle=shuffle
+    )
+    
+    return history
