2618b352d5de1734a7de16ee24e9bebee7d5e0df
rmecidli26 <rmecidli26@gmail.com>
2026-08-02 07:14:26 +0400

    '0-exercise'

---
+++ supervised_learning/optimization/10-Adam.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Module to set up Adam optimizer in TensorFlow"""
+import tensorflow as tf
+
+
+def create_Adam_op(alpha, beta1, beta2, epsilon):
+    """
+    Sets up the Adam optimization algorithm in TensorFlow
+
+    Parameters:
+        alpha: learning rate
+        beta1: weight used for the first moment
+        beta2: weight used for the second moment
+        epsilon: small number to avoid division by zero
+
+    Returns:
+        optimizer: tf.keras.optimizers.Adam instance
+    """
+    return tf.keras.optimizers.Adam(
+        learning_rate=alpha,
+        beta_1=beta1,
+        beta_2=beta2,
+        epsilon=epsilon
+    )
