910c849d3bda863b77e2f98f04f6f9256efd5646
rmecidli26 <rmecidli26@gmail.com>
2026-08-02 06:42:37 +0400

    0-exercise

--- pipeline/data_augmentation/1-crop.py
+++ pipeline/data_augmentation/1-crop.py
@@ -1,19 +1,8 @@
 #!/usr/bin/env python3
-"""
-TensorFlow ilə şəklin təsadüfi kəsilməsi (random crop) modulu.
-"""
+"""Module to crop an image randomly"""
 import tensorflow as tf
 
 
 def crop_image(image, size):
-    """
-    3D tensor şəklində verilmiş şəkli təsadüfi ölçülərlə kəsir.
-
-    Parameters:
-        image: kəsiləcək şəkli özündə saxlayan 3D tf.Tensor
-        size: kəsiləcək sahənin ölçüsünü göstərən k
-
-    Returns:
-        Təsadüfi kəsilmiş tf.Tensor
-    """
+    """Crops an image randomly to a given size"""
     return tf.image.random_crop(image, size=size)
