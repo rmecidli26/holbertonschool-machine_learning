b2ee529e4c4ea1e37e8ae0798a8b375771c0b532
rmecidli26 <rmecidli26@gmail.com>
2026-08-02 06:02:32 +0400

    0-exercise

---
+++ pipeline/data_augmentation/0-flip.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+TensorFlow ilə şəklin üfüqi çevrilməsi modulu.
+"""
+import tensorflow as tf
+
+
+def flip_image(image):
+    """
+    3D tens
+
+    Parameters:
+        im
+
+    Returns:
+        
+    """
+    return tf.image.flip_left_right(image)
