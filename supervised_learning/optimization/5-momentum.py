d67f2dadb9cb752aefd5fb2d61732918aeea18a2
rmecidli26 <rmecidli26@gmail.com>
2026-08-02 07:08:36 +0400

    '0-exercise'

---
+++ supervised_learning/optimization/5-momentum.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Module to update variables using """
+
+
+def update_variables_momentum(alpha, beta1, var, grad, v):
+    """
+    Updates a variable using 
+
+    Parameters:
+        alpha: learning rate
+        beta1: momentum weight
+        var: numpy.ndarray containing the variable to be updated
+        grad: numpy.ndarray containing the gradient of var
+        v: previous first moment of var
+
+    Returns:
+        var, v: the updated variable and
+    """
+    v = beta1 * v + (1 - beta1) * grad
+    var = var - alpha * v
+    return var, v
