2f9cb3775e6e3431c4fec5be6dc623812836a9bd
rmecidli26 <rmecidli26@gmail.com>
2026-08-04 07:33:40 +0400

    '0-exercise'

---
+++ supervised_learning/cnn/5-lenet5.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+"""Builds a modified version of LeNet-5 using Keras"""
+
+from tensorflow import keras as K
+
+
+def lenet5(X):
+    """Builds a modified
+
+    Args:
+        X: K.Input of shape (m, 28, 28, 1)
+
+    Returns:
+        a K.Model compiled
+    """
+    initializer = K.initializers.he_normal(seed=0)
+
+    conv1 = K.layers.Conv2D(
+        filters=6,
+        kernel_size=(5, 5),
+        padding='same',
+        activation='relu',
+        kernel_initializer=initializer
+    )(X)
+
+    pool1 = K.layers.MaxPooling2D(
+        pool_size=(2, 2),
+        strides=(2, 2)
+    )(conv1)
+
+    conv2 = K.layers.Conv2D(
+        filters=16,
+        kernel_size=(5, 5),
+        padding='valid',
+        activation='relu',
+        kernel_initializer=initializer
+    )(pool1)
+
+    pool2 = K.layers.MaxPooling2D(
+        pool_size=(2, 2),
+        strides=(2, 2)
+    )(conv2)
+
+    flatten = K.layers.Flatten()(pool2)
+
+    fc1 = K.layers.Dense(
+        units=120,
+        activation='relu',
+        kernel_initializer=initializer
+    )(flatten)
+
+    fc2 = K.layers.Dense(
+        units=84,
+        activation='relu',
+        kernel_initializer=initializer
+    )(fc1)
+
+    output = K.layers.Dense(
+        units=10,
+        activation='softmax',
+        kernel_initializer=initializer
+    )(fc2)
+
+    model = K.Model(inputs=X, outputs=output)
+
+    model.compile(
+        optimizer='adam',
+        loss='categorical_crossentropy',
+        metrics=['accuracy']
+    )
+
+    return model
