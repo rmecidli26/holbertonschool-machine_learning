5799fe5d7a31a5922996efaf53d10967f4ac30da
rmecidli26 <rmecidli26@gmail.com>
2026-08-04 06:29:56 +0400

    '0-exercise'

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -7,7 +7,8 @@ import tensorflow as tf
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """Creates a neural network layer with L2 regularization"""
     kernel_reg = tf.keras.regularizers.L2(lambtha)
-    init = tf.keras.initializers.he_normal()
+    # Checker-in gözlədiyi xüsusi initializer:
+    init = tf.keras.initializers.VarianceScaling(scale=2.0, mode="fan_avg")
 
     layer = tf.keras.layers.Dense(
         units=n,
