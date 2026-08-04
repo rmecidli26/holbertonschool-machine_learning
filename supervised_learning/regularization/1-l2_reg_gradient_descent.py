69985ffc4776c8002a6ddea12ffd525b097edd56
rmecidli26 <rmecidli26@gmail.com>
2026-08-04 06:22:51 +0400

    '0-exercise'

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -8,26 +8,23 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     """Updates network parameters
 
     Args:
-        Y: One-hot array of shape (classes, m).
-        alpha: Learning rate.
         lambtha: L2 regularization parameter.
         L: Number of layers in the neural network.
     """
     m = Y.shape[1]
     dz = cache[f'A{L}'] - Y
 
-    for l in range(L, 0, -1):
-        a_prev = cache[f'A{l-1}']
-        w_current = weights[f'W{l}']
+    for i in range(L, 0, -1):
+        a_prev = cache[f'A{i-1}']
+        w_current = weights[f'W{i}']
 
-        # Compute gradients including L2 regularization penalty for weights
+        # Compute gradients with L2 regularization
         dw = (1 / m) * np.matmul(dz, a_prev.T) + (lambtha / m) * w_current
         db = (1 / m) * np.sum(dz, axis=1, keepdims=True)
 
-        if l > 1:
-            # Backpropagate through tanh activation: g'(z) = 1 - A^2
+        if i > 1:
             dz = np.matmul(w_current.T, dz) * (1 - (a_prev ** 2))
 
-        # Update weights and biases in place
-        weights[f'W{l}'] -= alpha * dw
-        weights[f'b{l}'] -= alpha * db
+        # Update parameters in place
+        weights[f'W{i}'] -= alpha * dw
+        weights[f'b{i}'] -= alpha * db
