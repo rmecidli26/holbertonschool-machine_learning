4ba2227ab08fae45a526d1fc4c3bde0c374d9d31
rmecidli26 <rmecidli26@gmail.com>
2026-08-04 06:26:11 +0400

    '0-exercise'

---
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Creates a Keras Dense layer with L2 regularization"""
+
+import tensorflow as tf
+
+
+def l2_reg_create_layer(prev, n, activation, lambtha):
+    """Creates a neural network layer with L2"""
+    kernel_reg = tf.keras.regularizers.L2(lambtha)
+    layer = tf.keras.layers.Dense(
+        units=n,
+        activation=activation,
+        kernel_regularizer=kernel_reg
+    )
+    return layer(prev)
