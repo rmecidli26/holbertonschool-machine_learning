05fc1b0c867de8995ee55a40362e55562ca3d0d2
rmecidli26 <rmecidli26@gmail.com>
2026-08-21 01:45:03 +0000

    0-exercise

---
+++ supervised_learning/neural_style_transfer/1-neural_style.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+"""Neural Style Transfer Class Module."""
+import numpy as np
+import tensorflow as tf
+
+
+class NST:
+    """Performs tasks for Neural Style Transfer."""
+
+    style_layers = [
+        'block1_conv1',
+        'block2_conv1',
+        'block3_conv1',
+        'block4_conv1',
+        'block5_conv1'
+    ]
+    content_layer = 'block5_conv2'
+
+    def __init__(self, style_image, content_image, alpha=1e4, beta=1):
+        """Initialize Neural Style Transfer parameters and images.
+
+        Args:
+            style_image (np.ndarray): Image used as style reference
+            content_image (np.ndarray): Image used as content reference
+            alpha (float/int): Weight for content cost
+            beta (float/int): Weight for style cost
+        """
+        if not isinstance(style_image, np.ndarray) or \
+           style_image.ndim != 3 or style_image.shape[2] != 3:
+            raise TypeError(
+                "style_image must be a numpy.ndarray with shape (h, w, 3)"
+            )
+
+        if not isinstance(content_image, np.ndarray) or \
+           content_image.ndim != 3 or content_image.shape[2] != 3:
+            raise TypeError(
+                "content_image must be a numpy.ndarray with shape (h, w, 3)"
+            )
+
+        if not isinstance(alpha, (int, float)) or \
+           isinstance(alpha, bool) or alpha < 0:
+            raise TypeError("alpha must be a non-negative number")
+
+        if not isinstance(beta, (int, float)) or \
+           isinstance(beta, bool) or beta < 0:
+            raise TypeError("beta must be a non-negative number")
+
+        self.style_image = self.scale_image(style_image)
+        self.content_image = self.scale_image(content_image)
+        self.alpha = alpha
+        self.beta = beta
+        self.load_model()
+
+    @staticmethod
+    def scale_image(image):
+        """Rescales image pixel values to [0, 1] with max side 512.
+
+        Args:
+            image (np.ndarray): Image array with shape (h, w, 3)
+
+        Returns:
+            tf.Tensor: Scaled image tensor of shape (1, h_new, w_new, 3)
+        """
+        if not isinstance(image, np.ndarray) or \
+           image.ndim != 3 or image.shape[2] != 3:
+            raise TypeError(
+                "image must be a numpy.ndarray with shape (h, w, 3)"
+            )
+
+        h, w, _ = image.shape
+        if h > w:
+            h_new = 512
+            w_new = int(w * (512 / h))
+        else:
+            w_new = 512
+            h_new = int(h * (512 / w))
+
+        image = tf.expand_dims(image, axis=0)
+        resized_image = tf.image.resize(
+            image,
+            size=[h_new, w_new],
+            method=tf.image.ResizeMethod.BICUBIC
+        )
+        scaled_image = resized_image / 255.0
+        return tf.clip_by_value(scaled_image, 0.0, 1.0)
+
+    def load_model(self):
+        """Creates the model used to calculate Neural Style Transfer costs."""
+        vgg = tf.keras.applications.VGG19(
+            include_top=False,
+            weights='imagenet'
+        )
+
+        x = vgg.input
+        outputs = []
+
+        for layer in vgg.layers[1:]:
+            if isinstance(layer, tf.keras.layers.MaxPooling2D):
+                layer = tf.keras.layers.AveragePooling2D(
+                    pool_size=layer.pool_size,
+                    strides=layer.strides,
+                    padding=layer.padding,
+                    name=layer.name
+                )
+            layer.trainable = False
+            x = layer(x)
+            if layer.name in self.style_layers or \
+               layer.name == self.content_layer:
+                outputs.append(x)
+
+        model = tf.keras.Model(inputs=vgg.input, outputs=outputs)
+        model.trainable = False
+        self.model = model
