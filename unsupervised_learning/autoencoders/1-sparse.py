802bd3a6d1bad9e65acc21703539edd1a38d7932
rmecidli26 <rmecidli26@gmail.com>
2026-08-24 03:24:02 +0000

    0-exercises

---
+++ unsupervised_learning/autoencoders/1-sparse.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""Sparse Autoencoder implementation using TensorFlow Keras.
+"""
+
+import tensorflow.keras as K
+
+
+def autoencoder(input_dims, hidden_layers, latent_dims, lambtha):
+    """Creates a sparse autoencoder with L1 regularization on the latent space."""
+    # --- Encoder ---
+    input_enc = K.Input(shape=(input_dims,))
+    x = input_enc
+    for units in hidden_layers:
+        x = K.layers.Dense(units, activation='relu')(x)
+    
+    # Latent qatına L1 regularizasiyası (aktivliyə görə cərimə) əlavə edilir
+    latent = K.layers.Dense(
+        latent_dims,
+        activation='relu',
+        activity_regularizer=K.regularizers.l1(lambtha)
+    )(x)
+    encoder = K.Model(input_enc, latent, name='encoder')
+
+    # --- Decoder ---
+    input_dec = K.Input(shape=(latent_dims,))
+    x = input_dec
+    reversed_layers = hidden_layers[::-1]
+    for units in reversed_layers:
+        x = K.layers.Dense(units, activation='relu')(x)
+
+    # Sonuncu qat sigmoid aktivasiya funksiyasından istifadə edir
+    output_dec = K.layers.Dense(input_dims, activation='sigmoid')(x)
+    decoder = K.Model(input_dec, output_dec, name='decoder')
+
+    # --- Full Autoencoder ---
+    auto_input = K.Input(shape=(input_dims,))
+    latent_repr = encoder(auto_input)
+    reconstructed = decoder(latent_repr)
+    auto = K.Model(auto_input, reconstructed, name='autoencoder')
+
+    # Modeli adam optimizatoru və binary_crossentropy itki funksiyası ilə kompilyasiya edirik
+    auto.compile(optimizer='adam', loss='binary_crossentropy')
+
+    return encoder, decoder, auto
