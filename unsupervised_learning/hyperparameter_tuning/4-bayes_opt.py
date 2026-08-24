b8686b053ea078e9e8e78f132401bb9227c9b106
rmecidli26 <rmecidli26@gmail.com>
2026-08-24 02:59:24 +0000

    0-exercises

--- unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
@@ -22,8 +22,10 @@ class BayesianOptimization:
     def acquisition(self):
         """Calculate the next best sample location using Expected Improvement."""
         mu, sigma = self.gp.predict(self.X_s)
-        sigma = np.maximum(sigma, 1e-9)  # Avoid division by zero
-        sigma = np.sqrt(sigma)
+        
+        # Standard deviation qrafikində mənfi və ya sıfır qiymətlərin qarşısını almaq
+        sigma = np.maximum(sigma, 1e-9)
+        sigma_sig = np.sqrt(sigma)
 
         if self.minimize:
             current_optimum = np.min(self.gp.Y)
@@ -32,11 +34,11 @@ class BayesianOptimization:
             current_optimum = np.max(self.gp.Y)
             improvement = mu - current_optimum - self.xsi
 
-        Z = improvement / sigma
-        EI = improvement * norm.cdf(Z) + sigma * norm.pdf(Z)
+        Z = improvement / sigma_sig
+        EI = improvement * norm.cdf(Z) + sigma_sig * norm.pdf(Z)
 
-        # If sigma is 0, EI should be 0
-        EI[sigma == 0.0] = 0.0
+        # Standart kənarlaşma sıfır olduqda EI-ni sıfırlamaq
+        EI[sigma_sig == 0.0] = 0.0
 
         X_next = self.X_s[np.argmax(EI)]
         return X_next, EI
