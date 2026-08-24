ef64089c71e3ffe411fe4faf6ab4393ac372460a
rmecidli26 <rmecidli26@gmail.com>
2026-08-24 02:48:24 +0000

    0-exercises

---
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Gaussian Process implementation using NumPy.
+"""
+
+import numpy as np
+
+
+class GaussianProcess:
+    """Represents a noiseless 1D Gaussian process."""
+
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+        """Initialize the Gaussian Process."""
+        self.X = X_init
+        self.Y = Y_init
+        self.l = l
+        self.sigma_f = sigma_f
+        self.K = self.kernel(self.X, self.X)
+
+    def kernel(self, X1, X2):
+        """Calculate the covariance kernel matrix between two matrices
+        using the Radial Basis Function (RBF).
+        """
+        # RBF Kernel formula: K(x1, x2) = sigma_f^2 * exp(-0.5 * ||x1 - x2||^2 / l^2)
+        # Using squared Euclidean distance: ||x1 - x2||^2 = x1^2 + x2^2 - 2*x1*x2^T
+        sqdist = (
+            np.sum(X1**2, axis=1, keepdims=True)
+            + np.sum(X2**2, axis=1)
+            - 2 * np.dot(X1, X2.T)
+        )
+        return (self.sigma_f ** 2) * np.exp(-0.5 / (self.l ** 2) * sqdist)
