c1e9aa3dc9c49bca548fc54056f24a7631600759
rmecidli26 <rmecidli26@gmail.com>
2026-08-24 02:56:43 +0000

    0-exercises

---
+++ unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Bayesian Optimization implementation using a Gaussian Process.
+"""
+
+import numpy as np
+GP = __import__('2-gp').GaussianProcess
+
+
+class BayesianOptimization:
+    """Represents a Bayesian optimization on a noiseless 1D Gaussian process."""
+
+    def __init__(self, f, X_init, Y_init, bounds, ac_samples,
+                 l=1, sigma_f=1, xsi=0.01, minimize=True):
+        """Initialize the Bayesian Optimization."""
+        self.f = f
+        self.gp = GP(X_init, Y_init, l=l, sigma_f=sigma_f)
+        self.X_s = np.linspace(bounds[0], bounds[1], ac_samples).reshape(-1, 1)
+        self.xsi = xsi
+        self.minimize = minimize
