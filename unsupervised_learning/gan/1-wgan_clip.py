147277f8cfd3450cfd238a7e5c8d7d0d0cee9e16
rmecidli26 <rmecidli26@gmail.com>
2026-08-24 02:36:33 +0000

    0-exercises

---
+++ unsupervised_learning/gan/1-wgan_clip.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+"""Wasserstein GAN with weight clipping implementation using TensorFlow and Keras."""
+
+import tensorflow as tf
+from tensorflow import keras
+
+
+class WGAN_clip(keras.Model):
+    """Wasserstein GAN model with weight clipping."""
+
+    def __init__(self, generator, discriminator, latent_generator,
+                 real_examples, batch_size=200, disc_iter=2,
+                 learning_rate=0.005):
+        """Initialize the WGAN_clip model."""
+        super().__init__()
+        self.latent_generator = latent_generator
+        self.real_examples = real_examples
+        self.generator = generator
+        self.discriminator = discriminator
+        self.batch_size = batch_size
+        self.disc_iter = disc_iter
+
+        self.learning_rate = learning_rate
+        self.beta_1 = 0.5
+        self.beta_2 = 0.9
+
+        # Define generator loss and optimizer: opposite of the mean discriminator output on fake samples
+        self.generator.loss = lambda x: -tf.math.reduce_mean(x)
+        self.generator.optimizer = keras.optimizers.Adam(
+            learning_rate=self.learning_rate,
+            beta_1=self.beta_1,
+            beta_2=self.beta_2
+        )
+        self.generator.compile(
+            optimizer=generator.optimizer,
+            loss=generator.loss
+        )
+
+        # Define discriminator loss and optimizer: mean(disc(fake)) - mean(disc(real))
+        self.discriminator.loss = lambda x, y: tf.math.reduce_mean(x) - tf.math.reduce_mean(y)
+        self.discriminator.optimizer = keras.optimizers.Adam(
+            learning_rate=self.learning_rate,
+            beta_1=self.beta_1,
+            beta_2=self.beta_2
+        )
+        self.discriminator.compile(
+            optimizer=discriminator.optimizer,
+            loss=discriminator.loss
+        )
+
+    def get_fake_sample(self, size=None, training=False):
+        """Generate fake samples using the generator."""
+        if not size:
+            size = self.batch_size
+        return self.generator(self.latent_generator(size), training=training)
+
+    def get_real_sample(self, size=None):
+        """Get a random batch of real examples."""
+        if not size:
+            size = self.batch_size
+        sorted_indices = tf.range(tf.shape(self.real_examples)[0])
+        random_indices = tf.random.shuffle(sorted_indices)[:size]
+        return tf.gather(self.real_examples, random_indices)
+
+    def train_step(self, useless_argument):
+        """Perform a single training step for the WGAN."""
+        for _ in range(self.disc_iter):
+            with tf.GradientTape() as tape:
+                real_sample = self.get_real_sample()
+                fake_sample = self.get_fake_sample(training=True)
+                discr_loss = self.discriminator.loss(
+                    self.discriminator(fake_sample, training=True),
+                    self.discriminator(real_sample, training=True)
+                )
+            grads = tape.gradient(
+                discr_loss, self.discriminator.trainable_variables
+            )
+            self.discriminator.optimizer.apply_gradients(
+                zip(grads, self.discriminator.trainable_variables)
+            )
+
+            # Clip the weights of the discriminator between -1 and 1
+            for var in self.discriminator.trainable_variables:
+                var.assign(tf.clip_by_value(var, -1.0, 1.0))
+
+        with tf.GradientTape() as tape:
+            fake_sample = self.get_fake_sample(training=True)
+            gen_loss = self.generator.loss(
+                self.discriminator(fake_sample, training=True)
+            )
+        grads = tape.gradient(gen_loss, self.generator.trainable_variables)
+        self.generator.optimizer.apply_gradients(
+            zip(grads, self.generator.trainable_variables)
+        )
+
+        return {"discr_loss": discr_loss, "gen_loss": gen_loss}
