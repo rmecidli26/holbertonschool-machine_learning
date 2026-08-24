907052d8b030eb5b2ef4c12f45a31c2a7fc0a936
rmecidli26 <rmecidli26@gmail.com>
2026-08-24 02:57:15 +0000

    0-exercises

--- unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
@@ -7,7 +7,7 @@ GP = __import__('2-gp').GaussianProcess
 
 
 class BayesianOptimization:
-    """Represents a Bayesian optimization on a noiseless 1D Gaussian process."""
+    """Represents a Bayesian optimization on a noiseless"""
 
     def __init__(self, f, X_init, Y_init, bounds, ac_samples,
                  l=1, sigma_f=1, xsi=0.01, minimize=True):
