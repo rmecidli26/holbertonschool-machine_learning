347d07701b5f1d820bd18374263a6865f673c46d
rmecidli26 <rmecidli26@gmail.com>
2026-08-30 14:01:08 +0000

    0-exercise

---
+++ supervised_learning/RNNs/4-deep_rnn.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+Deep RNN module
+"""
+
+import numpy as np
+
+
+def deep_rnn(rnn_cells, X, h_0):
+    """Performs forward propagation for a deep RNN."""
+    t, m, i = X.shape
+    l, _, h = h_0.shape
+
+    # H shape: (t + 1, l, m, h)
+    H = np.zeros((t + 1, l, m, h))
+    H[0] = h_0
+
+    outputs = []
+
+    for step in range(t):
+        x_current = X[step]
+        for layer in range(l):
+            cell = rnn_cells[layer]
+            h_prev = H[step, layer]
+            # For the first layer, input is x_current; for subsequent layers, it's the hidden state of the layer below
+            if layer == 0:
+                h_next, y = cell.forward(h_prev, x_current)
+            else:
+                h_next, y = cell.forward(h_prev, H[step + 1, layer - 1])
+            H[step + 1, layer] = h_next
+            if layer == l - 1:
+                outputs.append(y)
+
+    Y = np.array(outputs)
+    return H, Y
