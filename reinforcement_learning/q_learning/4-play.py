5779690d48cd5035d1aff570d4027412bb2beed5
rmecidli26 <rmecidli26@gmail.com>
2026-08-31 18:05:54 +0000

    0-exercises

--- reinforcement_learning/q_learning/4-play.py
+++ reinforcement_learning/q_learning/4-play.py
@@ -16,18 +16,17 @@ def play(env, Q, max_steps=100):
         total_rewards: The total rewards for the episode.
         rendered_outputs: List of rendered outputs representing board states.
     """
-    state, _ = env.reset()
+    state = env.s
     rendered_outputs = [env.render()]
-    total_rewards = 0.0
+    total_rewards = 0
 
     for step in range(max_steps):
         action = np.argmax(Q[state])
-
         state, reward, terminated, truncated, _ = env.step(action)
         rendered_outputs.append(env.render())
+        total_rewards += reward
 
         if terminated or truncated:
-            total_rewards = reward
             break
 
     return total_rewards, rendered_outputs
