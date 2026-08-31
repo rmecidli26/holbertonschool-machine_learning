a15f3ba3978079e0956da268f732e7f0c582994b
rmecidli26 <rmecidli26@gmail.com>
2026-08-31 18:38:14 +0000

    0-exercises

---
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""Module to create, build, and train a Gensim Word2Vec model."""
+
+from gensim.models import Word2Vec
+
+
+def word2vec_model(
+    sentences,
+    vector_size=100,
+    min_count=5,
+    window=5,
+    negative=5,
+    cbow=True,
+    epochs=5,
+    seed=0,
+    workers=1,
+):
+    """Creates, builds, and trains a Gensim Word2Vec model.
+
+    Args:
+        sentences: list of sentences to be trained on.
+        vector_size: dimensionality of the embedding layer.
+        min_count: minimum number of occurrences of a word for use in training.
+        window: maximum distance between current and predicted word.
+        negative: size of negative sampling.
+        cbow: boolean to determine training type (True: CBOW, False: Skip-gram)
+        epochs: number of iterations to train over.
+        seed: seed for the random number generator.
+        workers: number of worker threads to train the model.
+
+    Returns:
+        The trained Word2Vec model.
+    """
+    sg = 0 if cbow else 1
+
+    model = Word2Vec(
+        sentences=sentences,
+        vector_size=vector_size,
+        min_count=min_count,
+        window=window,
+        negative=negative,
+        sg=sg,
+        epochs=epochs,
+        seed=seed,
+        workers=workers,
+    )
+
+    return model
