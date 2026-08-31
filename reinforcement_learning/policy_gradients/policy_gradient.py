63eb219356b26e75b474a25ebd9ce844fc69c2a8
rmecidli26 <rmecidli26@gmail.com>
2026-08-31 18:27:56 +0000

    0-exercises

--- reinforcement_learning/policy_gradients/policy_gradient.py
+++ reinforcement_learning/policy_gradients/policy_gradient.py
@@ -8,33 +8,38 @@ def policy(matrix, weight):
     """Computes the policy (action probabilities) using Softmax.
 
     Args:
-        matrix: numpy.ndarray of shape (1, n) representing the state.
-        weight: numpy.ndarray of shape (n, m) representing the weight matrix.
+        matrix: numpy.ndarray representing the state.
+        weight: numpy.ndarray of shape (n, m) representing
 
     Returns:
         numpy.ndarray of shape (1, m) containing the action probabilities.
     """
+    if matrix.ndim == 1:
+        matrix = matrix.reshape(1, -1)
     z = np.matmul(matrix, weight)
     exp_z = np.exp(z)
     return exp_z / np.sum(exp_z, axis=1, keepdims=True)
 
 
 def policy_gradient(state, weight):
-    """Computes the Monte-Carlo policy gradient based on a state and weight matrix.
+    """Computes the Monte-Carlo policy gradient for a state
 
     Args:
-        state: numpy.ndarray representing the current state observation.
-        weight: numpy.ndarray of shape (n, m) representing the weight matrix.
+        state: numpy.ndarray representing current state observation.
+        weight: numpy.ndarray of shape (n, m) representing
 
     Returns:
-        action: Sampled action based on the policy probabilities.
-        grad: The gradient of the log-policy with respect to the weight matrix.
+        action: Sampled action based on policy probabilities.
+        grad: Gradient of log-policy with respect to weight matrix.
     """
+    if state.ndim == 1:
+        state = state.reshape(1, -1)
+
     probs = policy(state, weight)
     action = np.random.choice(probs.shape[1], p=probs[0])
 
-    dsoftmax = probs.copy()
-    dsoftmax[0, action] -= 1
-    grad = np.matmul(state.T, -dsoftmax)
+    dsoftmax = -probs
+    dsoftmax[0, action] += 1
+    grad = np.matmul(state.T, dsoftmax)
 
     return action, grad
