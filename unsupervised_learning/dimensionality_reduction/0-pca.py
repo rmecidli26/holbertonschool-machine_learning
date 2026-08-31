9d3c86011c112cdb660ec2d2c15a1193b83cfcbf
rmecidli26 <rmecidli26@gmail.com>
2026-08-31 17:43:49 +0000

    0-exercises

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -1,19 +1,13 @@
 #!/usr/bin/env python3
+"""Performs Principal Component Analysis (PCA) on a dataset."""
+
 import numpy as np
 
 
 def pca(X, var=0.95):
-  """Performs PCA on a dataset keeping 'var' fraction of variance."""
-  # Compute SVD of X
+  """Performs PCA on X and returns weights maintaining var variance."""
   U, S, Vt = np.linalg.svd(X)
-
-  # Compute explained variance ratio
-  explained_variance = (S ** 2) / np.sum(S ** 2)
-  cumulative_variance = np.cumsum(explained_variance)
-
-  # Find number of components to reach 'var'
-  nd = np.argmax(cumulative_variance >= var) + 1
-
-  # Return the weights matrix W (transpose of first nd rows of Vt)
-  W = Vt[:nd, :].T
-  return W
+  var_ratio = (S ** 2) / np.sum(S ** 2)
+  cum_var = np.cumsum(var_ratio)
+  nd = np.np.argmax(cum_var >= var) + 1 if hasattr(np, 'np') else np.argmax(cum_var >= var) + 1
+  return Vt[:nd, :].T
