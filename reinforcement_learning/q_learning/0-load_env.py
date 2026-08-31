d74c0d65d56b5ec2d744322331e2ca9d5dfe9b3a
rmecidli26 <rmecidli26@gmail.com>
2026-08-31 17:58:13 +0000

    0-exercises

--- reinforcement_learning/q_learning/0-load_env.py
+++ reinforcement_learning/q_learning/0-load_env.py
@@ -5,29 +5,29 @@ import gymnasium as gym
 
 
 def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
-  """Loads the FrozenLakeEnv environment from gymnasium.
+    """Loads the FrozenLakeEnv environment from gymnasium.
 
-  Args:
-      desc: List of lists containing a custom map description.
-      map_name: String containing pre-made map name ('4x4' or '8x8').
-      is_slippery: Boolean to determine if ice is slippery.
+    Args:
+        desc: List of lists containing a custom map description.
+        map_name: String containing pre-made map
+        is_slippery: Boolean to determine if ice is slippery.
 
-  Returns:
-      The gymnasium FrozenLake environment instance.
-  """
-  if desc is None and map_name is None:
-    env = gym.make(
-        "FrozenLake-v1",
-        desc=None,
-        map_name="8x8",
-        is_slippery=is_slippery,
-    )
-  else:
-    env = gym.make(
-        "FrozenLake-v1",
-        desc=desc,
-        map_name=map_name,
-        is_slippery=is_slippery,
-    )
+    Returns:
+        The gymnasium FrozenLake environment instance.
+    """
+    if desc is None and map_name is None:
+        env = gym.make(
+            "FrozenLake-v1",
+            desc=None,
+            map_name="8x8",
+            is_slippery=is_slippery,
+        )
+    else:
+        env = gym.make(
+            "FrozenLake-v1",
+            desc=desc,
+            map_name=map_name,
+            is_slippery=is_slippery,
+        )
 
-  return env
+    return env
