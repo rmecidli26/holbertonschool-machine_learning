70b24f47acf745a87ae05f17aaf6747527d0f067
rmecidli26 <rmecidli26@gmail.com>
2026-08-31 17:59:12 +0000

    0-exercises

---
+++ reinforcement_learning/q_learning/1-q_init.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Module to initialize the Q-table for reinforcement learning."""
+
+import numpy as np
+
+
+def q_init(env):
+    """Initializes a Q-table with zeros for a given Gymnasium environment.
+
+    Args:
+        env: The FrozenLakeEnv instance.
+
+    Returns:
+        A numpy.ndarray of zeros with shape (action_space.n, observation_space.n).
+    """
+    action_space_size = env.action_space.n
+    state_space_size = env.observation_space.n
+
+    return np.zeros((state_space_size, action_space_size))
